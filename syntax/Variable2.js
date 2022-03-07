a77058949a5eb606ac02ff5cd764d4a8fa22be0e
soohwan97 <qkqhzz120@naver.com>
2022-03-07 12:04:40 +0900

    JS Template Literal and Nodejs url parse query string

--- syntax/Variable2.js
+++ syntax/Variable2.js
@@ -1,5 +1,9 @@
 var name = 'egoing';
 
-var letter = 'Lorem '+name+' ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore '+name+' magna aliqua. '+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo '+name+' consequat. Duis aute irure dolor in reprehenderit in voluptate egoing velit esse cillum dolore eu fugiat '+name+' nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in '+name+' culpa qui officia deserunt mollit anim id est laborum. '+name;
+var letter = 'Lorem '+name+'\n\n ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore '+name+' magna aliqua. '+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo '+name+' consequat. Duis aute irure dolor in reprehenderit in voluptate egoing velit esse cillum dolore eu fugiat '+name+' nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in '+name+' culpa qui officia deserunt mollit anim id est laborum. '+name;
+
+var letter = `Lorem ${name}
+
+ ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore ${name} magna aliqua. ${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ${name} consequat. Duis aute irure dolor in reprehenderit in voluptate egoing velit esse cillum dolore eu fugiat ${1+1} nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in ${name} culpa qui officia deserunt mollit anim id est laborum. ${name}`;
 
 console.log(letter);
